6412518b8b19ab9200a9fa98056d459055c942ba
Kapil Varunshri <kapilila@yahoo.com>
2017-10-22 20:03:29 -0400

    Added further code and initial form validation

--- app.js
+++ app.js
@@ -1,7 +1,23 @@
 const express = require('express');
 const exphbs  = require('express-handlebars');
+const bodyParser = require('body-parser');
+const mongoose = require('mongoose');
 
 const app = express();
+
+// Map global promise
+mongoose.Promise = global.Promise;
+
+// Connect to mongoose
+mongoose.connect('mongodb://localhost/memoryApp-dev', {
+    useMongoClient: true
+})
+    .then(() => console.log('MongoDB connected...'))
+    .catch(err => console.log(err));
+
+// Load Idea Model
+require('./models/Idea');
+const Idea = mongoose.model('ideas');
 
 // Handlebars middleware
 app.engine('handlebars', exphbs({
@@ -9,12 +25,16 @@ app.engine('handlebars', exphbs({
 }));
 app.set('view engine', 'handlebars');
 
+// Body parser middleware
+app.use(bodyParser.urlencoded({ extended: false }))
+app.use(bodyParser.json())
+
 // Middleware working
-app.use(function(req, res, next){
-    console.log(Date.now());
-    req.name = 'Kapil V';
-    next();
-});
+// app.use(function(req, res, next){
+//     //console.log(Date.now());
+//     req.name = 'Kapil V';
+//     next();
+// });
 
 // Index Route
 app.get('/', (req, res) => {
@@ -29,6 +49,44 @@ app.get('/', (req, res) => {
 // About Route
 app.get('/about', (req, res) => {
     res.render('about');
+});
+
+// Add Idea Form
+app.get('/ideas/add', (req, res) => {
+    res.render('ideas/add');
+});
+
+// Process form
+app.post('/ideas', (req, res) => {
+    // console.log(req.body);
+    // res.send('ok');
+    let errors = [];
+
+    if(!req.body.title){
+        errors.push({text:'Please add a title'});
+    }
+    if(!req.body.details){
+        errors.push({text:'Please add some details'});
+    }
+
+    if(errors.length > 0){
+        res.render('ideas/add', {
+            errors: errors,
+            title: req.body.title,
+            details: req.body.details
+        });
+    } else {
+        //res.send('passed');
+        const newUser = {
+            title: req.body.title,
+            details: req.body.details
+        }
+        new Idea(newUser)
+            .save()
+            .then(idea => {
+                res.redirect('/ideas');
+            })
+    }
 });
 
 const port = 5000;
